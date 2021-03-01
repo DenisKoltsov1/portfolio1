ea9ce2a32c75bbdd23506e8b9b5e90291dcea80b
DenisKoltsov1 <dublin87@mail.ru>
2021-03-01 16:12:54 +0300

    first commit

---
+++ components/dv/date.current/description.php
@@ -0,0 +1,22 @@
+<? if  (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$arComponentDescription=array(
+"NAME"=>GetMessage("текущая дата"),
+"DESCRIPTION"=>GetMessage("Выводим текущую дату"),
+"PATH"=>array(
+    "ID"=>"dv_components",
+    "CHILD"=>array(
+        "ID"=>"curdate",
+        "NAME"=>"екущая дата"
+
+    )
+    ),
+"ICON"=>"/image/iconf.gif",
+
+);
+
+
+
+
+
+
+?>
